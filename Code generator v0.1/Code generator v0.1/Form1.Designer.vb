43e0ab1f654e3e32d09932e8eba6a671f408b061
anshbansal <aseembansal@ymail.com>
2013-08-29 21:45:29 +0530

    v0.1 of Cpmag

---
+++ Code generator v0.1/Code generator v0.1/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.HelToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TechnicalToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VersionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HelToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(593, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'HelToolStripMenuItem
+        '
+        Me.HelToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem, Me.TechnicalToolStripMenuItem, Me.VersionToolStripMenuItem})
+        Me.HelToolStripMenuItem.Name = "HelToolStripMenuItem"
+        Me.HelToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(189, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'TechnicalToolStripMenuItem
+        '
+        Me.TechnicalToolStripMenuItem.Name = "TechnicalToolStripMenuItem"
+        Me.TechnicalToolStripMenuItem.Size = New System.Drawing.Size(189, 22)
+        Me.TechnicalToolStripMenuItem.Text = "Technical Details"
+        '
+        'VersionToolStripMenuItem
+        '
+        Me.VersionToolStripMenuItem.Name = "VersionToolStripMenuItem"
+        Me.VersionToolStripMenuItem.Size = New System.Drawing.Size(189, 22)
+        Me.VersionToolStripMenuItem.Text = "Adding More features"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(593, 362)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "C Code Generator v0.1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents HelToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents VersionToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TechnicalToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Code generator v0.1/Code generator v0.1/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+
+    Private Sub AboutToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutToolStripMenuItem.Click
+        MessageBox.Show("How can this app help you:-" &
+               Chr(13) &
+               "It can give you the C codes that you are looking for" &
+               Chr(13) & Chr(13) &
+               "Developer :" &
+               Chr(13) &
+               "Aseem Bansal" &
+               Chr(13) & Chr(13) &
+               "Project Started on:-" &
+               Chr(13) &
+               "24th August 2012",
+               "About"
+               )
+    End Sub
+
+    Private Sub VersionToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles VersionToolStripMenuItem.Click
+        MessageBox.Show("This is still a work in progress so" &
+               Chr(13) &
+               "If you want more features JUST send an me an e-mail about what you want " &
+               Chr(13) &
+               "and I will add them as soon as I can" &
+               Chr(13) & Chr(13) &
+               "NOTE:-" &
+               Chr(13) &
+               "Please write" &
+               Chr(13) &
+               "[C Code generator v0.1]" &
+               Chr(13) &
+               "in the title of the e-mail" &
+               Chr(13) & Chr(13) &
+               "CONTACT:-" &
+               Chr(13) &
+               "aseembansal@ymail.com",
+               "Adding more features to this")
+    End Sub
+
+    Private Sub TechnicalToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TechnicalToolStripMenuItem.Click
+        MessageBox.Show("Tool on which the Codes have been tested" &
+               Chr(13) &
+               "1. Code:Blocks (An Open Source C/C++ IDE)  http://www.codeblocks.org/", "Technical Detials"
+               )
+    End Sub
+
+End Class
